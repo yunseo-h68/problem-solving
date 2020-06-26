7bd1ba3dfec71430eba2bbf89a9bbdb4bec31522
Yunseo Hwang <yunseo.h68@gmail.com>
2020-06-26 11:12:27 +0900

    Simplify 2941_크로아티아알파벳

--- BOJ/2941_크로아티아알파벳.cpp
+++ BOJ/2941_크로아티아알파벳.cpp
@@ -2,61 +2,29 @@
 #include <cstring>
 
 void fc(char* t, char* s, int a, int b) {
-	int i = 0;
-	
-	for(i = 0; i < 5; i++) {
-		t[i] = '\0';
-	}
-	
-	for(i = a; i < a+b; i++) {
-		t[i - a] = s[i];
-	}
+	for(int i = 0; i < 5; i++) t[i] = '\0';
+	for(int i = a; i < a+b; i++) t[i - a] = s[i];
 }
 
 int main() {
 	char s[101] = {0,};
 	char t[5];
-	int res = 0, ch=0;
+	int res = 0;
 	
-	char cro[7][3] = {
-		"c=",
-		"c-",
-		"d-",
-		"lj",
-		"nj",
-		"s=",
-		"z=",
-	};
 	scanf("%s", s);
 	
 	for(int i = 0; s[i]; i++) {
 		fc(t, s, i, 2);
 		
-		for(int j = 0; j < 7; j++) {
-			if(strcmp(t, cro[j])==0){
-				res++;
-				ch = 1;
-				i+=1;
-				
-				break;
-			}
+		if(t[1] == '=' && ( t[0] == 'c' || t[0] == 's' || t[0] == 'z')) i++;
+		else if(t[1] == '-' && (t[0] == 'c' || t[0] == 'd')) i++;
+		else if(t[1] == 'j' && (t[0] == 'l' || t[0] == 'n')) i++;
+		else {
+			fc(t,s,i,3);
+			if(!strcmp(t, "dz=")) i+=2;	
 		}
-		if(ch) {
-			ch = 0;
-			continue;
-		}
-		if(!ch) {
-			fc(t, s, i, 3);
-			if(strcmp(t, "dz=")==0) {
-				res++;
-				i+=2;
-				
-				continue;
-			}
-		}
-		
 		res++;
-		ch = 0;
 	}
+	
 	printf("%d", res);
 }
