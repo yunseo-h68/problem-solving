7ff2deff0059068dbdb27dcbc7dd6d245fe22f94
Yunseo Hwang <yunseo.h68@gmail.com>
2020-06-26 12:07:01 +0900

    Simplify 2941_크로아티알파벳

--- BOJ/2941_크로아티아알파벳.cpp
+++ BOJ/2941_크로아티아알파벳.cpp
@@ -1,30 +1,16 @@
 #include <cstdio>
-#include <cstring>
 
-void fc(char* t, char* s, int a, int b) {
-	for(int i = 0; i < 5; i++) t[i] = '\0';
-	for(int i = a; i < a+b; i++) t[i - a] = s[i];
-}
-
+char i, j, k;
+	
 int main() {
-	char s[101] = {0,};
-	char t[5];
-	int res = 0;
-	
-	scanf("%s", s);
-	
-	for(int i = 0; s[i]; i++) {
-		fc(t, s, i, 2);
-		
-		if(t[1] == '=' && ( t[0] == 'c' || t[0] == 's' || t[0] == 'z')) i++;
-		else if(t[1] == '-' && (t[0] == 'c' || t[0] == 'd')) i++;
-		else if(t[1] == 'j' && (t[0] == 'l' || t[0] == 'n')) i++;
-		else {
-			fc(t,s,i,3);
-			if(!strcmp(t, "dz=")) i+=2;	
-		}
-		res++;
+	int t = 0;
+	while((i=getchar()) != '\n') {
+		if (i == '=' && (j == 'c' || j == 's' || j == 'z'))
+			(j=='z'&&k=='d')?t--:0;
+		else if(
+		!(i == '-' && (j == 'c' || j == 'd')) &&
+		!(i == 'j' && (j == 'l' || j == 'n'))) t++;
+		k = j, j = i;
 	}
-	
-	printf("%d", res);
+	printf("%d", t);
 }
